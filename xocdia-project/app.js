22175bf86104d71a5f43b703badfd295279297ef
thedeadsheep <tranvietmar15@gmail.com>
2023-01-26 09:28:06 +0700

    thay doi de tot hon

--- xocdia-project/app.js
+++ xocdia-project/app.js
@@ -43,21 +43,3 @@ function ngauNhien() {
     console.log(typeof intRand)
     return intRand + 1
 }
-window.onload = function () {
-
-    //create a new instance of shake.js.
-    var myShakeEvent = new Shake({
-        threshold: 15
-    });
-
-    // start listening to device motion
-    myShakeEvent.start();
-
-    // register a shake event
-    window.addEventListener('shake', shakeEventDidOccur, false);
-
-    //shake event callback
-    function shakeEventDidOccur() {
-        alert("asdasd")
-    }
-};
